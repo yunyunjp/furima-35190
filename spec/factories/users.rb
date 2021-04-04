920579880bbf4a0773fe6a30fe732dcb93e907a9
yunyunjp <78527774+yunyunjp@users.noreply.github.com>
2021-04-04 17:38:43 +0900

    Userモデル単体テスト修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,8 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname {Faker::Name.last_name}
     email {Faker::Internet.free_email}
-    password { '1a' + Faker::Internet.password(min_length: 6)
-    password {password}
+    password { '1a' + Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
     last_name {'田中'}
     first_name {'太郎'}
