f49ae958ba79446cbbf027ce1e32a9ef81f65a0d
yunyunjp <78527774+yunyunjp@users.noreply.github.com>
2021-04-05 18:58:53 +0900

    商品出品機能実装修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -4,9 +4,7 @@ window.addEventListener('load', () => {
      const inputValue = priceInput.value;
      const addTaxDom = document.getElementById("add-tax-price")
      addTaxDom.innerHTML = (Math.floor(inputValue * 0.1));
-     console.log(addTaxDom.innerHTML);
      const saleProfit = document.getElementById("profit");
      saleProfit.innerHTML = (Math.floor(inputValue - (addTaxDom.innerHTML)));
-     console.log(saleProfit.innerHTML);
   });
 });
