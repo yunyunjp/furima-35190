dddbefae6d01879fbeae9a1d1736567aa3fe425b
yunyunjp <78527774+yunyunjp@users.noreply.github.com>
2021-04-05 18:59:18 +0900

    Itemモデル単体テスト修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -29,22 +29,27 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Info can't be blank")
       end
       it 'カテゴリーの情報が必須であること' do
-        @item.category_id = "1"
+        @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Category must be other than 1")
       end
       it '商品の状態についての情報が必須であること' do
-        @item.status_id = "1"
+        @item.status_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Status must be other than 1")
       end
       it '配送料の負担についての情報が必須であること' do
-        @item.fee_id = "1"
+        @item.fee_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Fee must be other than 1")
       end
       it '発送元の地域についての情報が必須であること' do
-        @item.delivery_id = "1"
+        @item.prefecture_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
+      end
+      it '発送までの日数についての情報が必須であること' do
+        @item.delivery_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Delivery must be other than 1")
       end
@@ -54,12 +59,12 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Price can't be blank", "Price is invalid", "Price is not a number")
       end
       it '販売価格は、¥300以上で保存可能であること' do
-        @item.price = "299"
+        @item.price = 299
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
       end
       it '販売価格は、¥9,999,999以下で保存可能であること' do
-        @item.price = "10000000"
+        @item.price = 10000000
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end
@@ -67,6 +72,16 @@ RSpec.describe Item, type: :model do
         @item.price = "３００"
         @item.valid?
         expect(@item.errors.full_messages).to include("Price is not a number")
+      end  
+      it '販売価格は価格は半角英語だけでは登録できないこと' do
+        @item.price = "aaa"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end  
+      it '販売価格は半角英数混合では登録できないこと' do
+        @item.price = "01a"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")        
       end
     end
   end
