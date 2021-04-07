31f452eca8aad2cb49f65e3da5c6fc61b26d72ec
yunyunjp <78527774+yunyunjp@users.noreply.github.com>
2021-04-07 10:31:23 +0900

    商品情報編集機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,9 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    unless current_user.id == @item.user_id
+      redirect_to action: :index
+    end  
   end
 
   def update
